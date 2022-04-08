217bfc1fb369712928ec0b6c1c05189686b96799
Bruno Yamada <brunofernandoyamada@gmail.com>
2022-04-08 00:36:34 -0300

    Adding Order creation log

--- src/main/java/br/com/byamada/awsservicesapringboot/model/Order.java
+++ src/main/java/br/com/byamada/awsservicesapringboot/model/Order.java
@@ -2,10 +2,13 @@ package br.com.byamada.awsservicesapringboot.model;
 
 import br.com.byamada.awsservicesapringboot.dto.OrderRequest;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Slf4j
 @Data
 @Entity
 public class Order {
@@ -25,6 +28,7 @@ public class Order {
 
     public Order(OrderRequest orderRequest) {
         UUID uuid = UUID.randomUUID();
+        log.info("Generating new OrderNumber: {}", uuid);
         this.orderNumber = uuid;
         this.document = orderRequest.getDocument();
         this.amount = orderRequest.getAmount();
